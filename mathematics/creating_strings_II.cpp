ee835f491dba35abb66545daec3eb633579706d7
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-02-18 23:05:56 +0700

    ok

---
+++ mathematics/creating_strings_II.cpp
@@ -0,0 +1,172 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://cses.fi/problemset/task/1715/
+*/
+#pragma GCC optimize("O3,unroll-loops")
+ 
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits(x) __builtin_popcountll(x)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+// #define ThinhNgo
+#ifdef ThinhNgo
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
+ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
+ll expo(ll a, ll b, ll mod) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll expo(ll a, ll b) {ll res = 1; while (b > 0) {if (b & 1)res = res * a; a = a * a; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {assert(gcd(a, b) == 1); return expo(a, b - 2, b);} // inverse mod: a^-1 % b = a^(b-2) % b. When gcd(a,b)=1 (or b prime)
+bool revsort(ll a, ll b) {return a > b;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+struct Combinatoric {
+    ll mxN;
+    vector<ll> fact;
+    vector<ll> ifact;
+    const int mod = 1e9 + 7;
+    Combinatoric(ll mxN) {
+        this->mxN = mxN;
+        fact.resize(mxN+1);
+        ifact.resize(mxN+1);
+        pre_compute();
+    }
+    // O(n): pre_compute factorial and inverse factorial of mxN
+    void pre_compute() {
+        fact[0] = 1;
+        for (ll i = 1; i <= this->mxN; i++) {
+            fact[i] = mod_mul(i, fact[i-1], this->mod);
+        }
+        ifact[this->mxN] = mminvprime(fact[this->mxN], this->mod);
+        for (ll i = this->mxN - 1; i >= 0; i--) {
+            ifact[i] = mod_mul(ifact[i + 1], i + 1, this->mod);
+        }
+    }
+    vector<ll> getFactArray() {
+        return this->fact;
+    }
+    vector<ll> getIFactArray() {
+        return this->ifact;
+    }
+
+    // C(n,r): number of ways to choose r items among n unique items
+    // require: pre_compute of fact[mxN] and ifact[mxN] (factorial and inverse factorial)
+    // require: r <= n 
+    // O(1)
+    ll combination1(ll n, ll r) {
+        return mod_mul(fact[n], mod_mul(ifact[r], ifact[n - r], this->mod), this->mod);
+    }
+};
+// C(n,r): in O(r + log(mod)) each time
+// Use when call C(n,r) a few times or n is to large (r is still acceptable)
+ll combination_Or(ll n, ll r, ll mod) {
+    ll numerator = 1;
+    ll denominator = 1;
+    for (ll i = n; i >= n - r + 1; i--) {
+        numerator = mod_mul(numerator, i, mod);
+    }
+    for (ll i = 1; i <= r; i++) {
+        denominator = mod_mul(denominator, i, mod);
+    }
+    ll i_denominator = mminvprime(denominator, mod);
+    return mod_mul(numerator, i_denominator, mod);
+}
+
+string s;
+void solve() {
+    cin >> s;
+    int n = s.size();
+    Combinatoric comb(n);
+    vector<ll> fact = comb.getFactArray();
+    vector<ll> ifact = comb.getIFactArray();
+    vector<int> cnt(26,0);
+    for (auto c : s) {
+        cnt[c-'a']++;
+    }
+    ll ans = fact[n];
+    for (auto c : cnt) {
+        if (c > 1) ans = mod_mul(ans,ifact[c],MOD);
+    }
+    cout << ans << nline;
+}
+
+int main() {
+#ifdef ThinhNgo
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+    IN_OUT();
+    auto start1 = high_resolution_clock::now();
+    solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef ThinhNgo
+    cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+}
