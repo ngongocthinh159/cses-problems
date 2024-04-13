62a8654ae9dcd531a2522967a7a5a9ad6ee2229e
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-04-13 19:14:04 +0700

    ok

---
+++ string_algorithms/reapeating_substring.cpp
@@ -0,0 +1,216 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://cses.fi/problemset/task/2106/
+*/
+#pragma GCC optimize("O3,unroll-loops")
+ 
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits(x) __builtin_popcountll(x)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+// #define ThinhNgo
+#ifdef ThinhNgo
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll __gcd__(ll a, ll b) {if (!a || !b) return a | b; unsigned shift = __builtin_ctz(a | b); a >>= __builtin_ctz(a); do { b >>= __builtin_ctz(b); if (a > b) swap(a, b); b -= a; } while (b); return a << shift;} // only a >= 0 && b >= 0
+ll gcd(ll a, ll b) {a = abs(a); b = abs(b); return __gcd__(a, b);} // get abs(a), abs(b) in case a < 0 || b < 0
+ll expo(ll a, ll b, ll mod) {if (a==0) return 1%mod; ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+ll expo(ll a, ll b) {ll res = 1; while (b > 0) {if (b & 1)res = res * a; a = a * a; b = b >> 1;} return res;} // a = 0 return 0 | b = 0 return 1
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {assert(gcd(a, b) == 1); return expo(a, b - 2, b);} // inverse mod: a^-1 % b = a^(b-2) % b. When gcd(a,b)=1 (or b prime)
+bool revsort(ll a, ll b) {return a > b;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+/*--------------------------------------------------------------------------------------------------------------------------*/
+// #define ThinhNgo_use_cases
+
+// Random base at each start
+int gen_base(const int before, const int after) {
+    int base = getRandomNumber(before + 1, after);
+    return base % 2 == 0 ? base - 1 : base;
+}
+
+struct PolyHash {
+    // -------- Static variables --------
+    static const int mod = (int)1e9 + 123;  // prime mod of polynomial hashing
+    static vector<int> pow1;                // powers of base modulo mod
+    static vector<ull> pow2;                // powers of base modulo 2^64
+    static int base;                        // base (point of hashing)
+
+    // --------- Static functons --------
+    static inline int diff(int a, int b) {
+        // Diff between `a` and `b` modulo mod (0 <= a < mod, 0 <= b < mod)
+        return (a -= b) < 0 ? a + mod : a;
+    }
+
+    // -------------- Variables of class -------------
+    vector<int> pref1;  // Hash on prefix modulo mod
+    vector<ull> pref2;  // Hash on prefix modulo 2^64
+
+    // Cunstructor from string:
+    PolyHash(const string& s) {
+        assert(base < mod);
+        const int n = s.size();  // Firstly calculated needed power of base:
+        while ((int)pow1.size() <= n) {
+            pow1.push_back(1LL * pow1.back() * base % mod);
+            pow2.push_back(pow2.back() * base);
+        }
+
+        // Initilize prefix hash for each mod: pref[k] = prefix hash of first k elements
+        pref1 = vector<int>(s.size() + 1u, 0);
+        pref2 = vector<ull>(s.size() + 1u, 0);
+        for (int i = 0; i < n; ++i) {  // Fill arrays with polynomial hashes on prefix
+            assert(base > s[i]);
+            pref1[i + 1] = (pref1[i] + 1LL * s[i] * pow1[i]) % mod;
+            pref2[i + 1] = pref2[i] + s[i] * pow2[i];  // C++ auto % 2^64 for ull
+        }
+    }
+
+    // Polynomial hash of subsequence [pos, pos+len)
+    // If mxPow != 0, value automatically multiply on base in needed power. Finally base ^ mxPow
+    // pos: is index in the string
+    // mxPow: max length of all the strings, specify when call this function
+    inline pair<int, ull> operator()(const int pos, const int len, const int mxPow) const {
+        int hash1 = pref1[pos + len] - pref1[pos];
+        ull hash2 = pref2[pos + len] - pref2[pos];
+        if (hash1 < 0) hash1 += mod;
+        if (mxPow != 0) {
+            hash1 = 1LL * hash1 * pow1[mxPow - (pos + len - 1)] % mod;
+            hash2 *= pow2[mxPow - (pos + len - 1)];
+        }
+        return make_pair(hash1, hash2);
+    }
+};
+
+// Init static variables of PolyHash class:
+int PolyHash::base((int)1e9 + 7);
+vector<int> PolyHash::pow1{1};
+vector<ull> PolyHash::pow2{1};
+
+void pre_compute() {
+
+}
+
+
+string s;
+void solve() {
+    cin >> s;
+    const int mxPow = s.size(); // phai khai bao khi tinh hash, mxPow la max(s.len)
+    PolyHash::base = gen_base(256, PolyHash::base); // gen random base [256->1e9 + 7]
+    PolyHash hash(s);
+    int n = s.size();
+    int l = 0, r = n;
+    pair<int,ull> repeatHash;
+    while (r - l > 1) {
+        int m = l + (r - l)/2;
+        bool ok = false;
+        vector<pair<int,ull>> v;
+        int len = m;
+        for (int hi = len - 1; hi < n; hi++) {
+            v.push_back(hash(hi - len + 1, len, mxPow));
+        }
+        sort(all(v));
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] == v[i - 1]) {ok = true; repeatHash = v[i]; break;}
+        }
+        
+        if (ok) {
+            l = m;
+        } else
+            r = m;
+    }
+    if (l == 0) cout << -1 << nline;
+    else {
+        for (int hi = l - 1; hi < n; hi++) {
+            if (hash(hi - l + 1, l, mxPow) == repeatHash) {
+                cout << s.substr(hi - l + 1, l) << nline; 
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+#ifdef ThinhNgo
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+    IN_OUT();
+    auto start1 = high_resolution_clock::now();
+    int T = 1;
+#ifdef ThinhNgo_use_cases
+    cin >> T;
+#endif
+    pre_compute();
+    while (T--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef ThinhNgo
+    cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+}
