9c1c886b606c010093cb87f0c6111012a644a055
ngongocthinh159 <ngongocthinh124@gmail.com>
2025-09-14 16:28:18 +0700

    ok

---
+++ bitwise_operations/Counting_Bits.cpp
@@ -0,0 +1,77 @@
+/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://cses.fi/problemset/task/1146/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+void pre_compute() {
+    
+}
+ll cnt_bits(ll n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    if (((n + 1) & n) == 0)
+        return 2 * cnt_bits(n / 2) + (n + 1) / 2;
+    
+    int msb = 64 - __builtin_clzll(n) - 1;
+    ll x = MASK(msb) - 1;
+    ll len = n - (x + 1) + 1;
+    return 2 * cnt_bits(x / 2) + (x + 1) / 2 + cnt_bits(len - 1) + len;
+}
+void solve() {
+	ll n; cin >> n;
+    cout << cnt_bits(n) << nline;
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    // cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
---
+++ bitwise_operations/SOS_Bit_Problem.cpp
@@ -0,0 +1,82 @@
+;/**
+ * Author: Thinh Ngo Ngoc
+ * Solution for: https://cses.fi/problemset/task/1654/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nline '\n'
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+#define LINF ((long long)1e18)
+#define IINF ((int)1e9)
+#define MASK(i) (1LL << (i))
+#define all(v) (v).begin(), (v).end()
+
+
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if (x > y) {
+        x = y;
+        return true;
+    } else return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if (x < y) {
+        x = y;
+        return true;
+    } else return false;
+}
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+void IN_OUT() {
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("Output.txt", "w", stdout);
+#endif
+}
+struct chash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(uint64_t x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}}; // https://codeforces.com/blog/entry/62393
+struct chashp {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}size_t operator()(pair<uint64_t,uint64_t> x) const {static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);}}; // https://codeforces.com/blog/entry/62393
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#define N 200005
+#define BIT 20
+int n, x[N];
+int sub[1 << BIT], sup[1 << BIT];
+void pre_compute() {
+    
+}
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> x[i], sub[x[i]]++, sup[x[i]]++;
+	for (int i = 0; i < BIT; i++) {
+		for (int state = 0; state < (1 << BIT); state++) {
+			if ((state >> i) & 1)
+				sub[state] += sub[state ^ (1 << i)];
+			else
+				sup[state] += sup[state ^ (1 << i)];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << sub[x[i]] << ' ';
+		cout << sup[x[i]] << ' ';
+		cout << (n - sub[(1 << BIT) - 1 - x[i]]) << ' ';
+		cout << nline;
+	}
+}
+
+int main() {
+    fastio();
+    IN_OUT();
+    int T = 1;
+    // cin >> T;
+    pre_compute();
+    for (int cases = 1; cases <= T; cases++) {
+
+        solve();
+    }
+    return 0;
+}
